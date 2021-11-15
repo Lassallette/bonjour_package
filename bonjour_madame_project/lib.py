d7ac460a300fbff35962c2c11eaf60c14c32ea44
Antoine Lassallette <antoine_lassallette@msn.com>
2021-11-15 16:43:10 +0100

    done

--- bonjour_madame_project/lib.py
+++ bonjour_madame_project/lib.py
@@ -1,4 +1,8 @@
 import webbrowser
 
 def get_today_picture():
-    webbrowser.open('https://www.bonjourmadame.fr/')
+    # webbrowser.open('https://www.bonjourmadame.fr/')
+    print("salooppe")
+
+if __name__=="__main__":
+    get_today_picture()
